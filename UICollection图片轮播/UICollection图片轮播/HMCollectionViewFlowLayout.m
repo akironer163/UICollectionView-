1a0232325850b70f9e995c6c9315c0607254966b
xianing <1120814158@qq.com>
2016-08-21 11:39:35 +0800

    添加UICollectionView 和 pageControl

---
+++ UICollection图片轮播/UICollection图片轮播/HMCollectionViewFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  HMCollectionViewFlowLayout.m
+//  UICollection图片轮播
+//
+//  Created by user on 16/8/21.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import "HMCollectionViewFlowLayout.h"
+
+@implementation HMCollectionViewFlowLayout
+
+@end
--- UICollection图片轮播/UICollection图片轮播/HMCycleView.m
+++ UICollection图片轮播/UICollection图片轮播/HMCycleView.m
@@ -7,15 +7,50 @@
 //
 
 #import "HMCycleView.h"
+#import "HMCollectionViewFlowLayout.h"
+#import "Masonry.h"
 
 @implementation HMCycleView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setupUI];
+    }
+    return self;
 }
-*/
+
+#pragma mark - 搭建界面
+- (void)setupUI {
+    
+    //1.添加collectionView
+    HMCollectionViewFlowLayout *flowLayout = [[HMCollectionViewFlowLayout alloc] init];
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flowLayout];
+    collectionView.backgroundColor = [UIColor redColor];
+    
+    [self addSubview:collectionView];
+    
+    [collectionView mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.left.right.equalTo(self);
+        make.bottom.equalTo(self).offset(-20);
+    }];
+    
+    //2.添加pageContrl
+    UIPageControl *pageContrl = [[UIPageControl alloc] init];
+    pageContrl.pageIndicatorTintColor = [UIColor colorWithWhite:0.8 alpha:1.0];
+    pageContrl.currentPageIndicatorTintColor = [UIColor colorWithWhite:0.2 alpha:1.0];
+    pageContrl.numberOfPages = 5;
+    
+    [self addSubview:pageContrl];
+    
+    [pageContrl mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.centerX.equalTo(collectionView);
+        make.top.equalTo(collectionView.mas_bottom).offset(-8);
+    }];
+    
+}
 
 @end
